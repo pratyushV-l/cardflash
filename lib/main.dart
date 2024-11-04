d52f885dca9269cee5bcd3a0edd2cfa57da07c06
Pratyush Vel Shankar <pratyushvel@outlook.com>
2024-11-04 20:17:19 +0530

    working, homepage done

--- lib/main.dart
+++ lib/main.dart
@@ -10,100 +10,93 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
-        ),
-        home: Scaffold(
-          body: ListView(children: const [
-            Page1(),
-          ]),
-        ),
-      );
+      theme: ThemeData.dark(),
+      home: const Scaffold(
+        body: Page1(),
+      ),
+    );
   }
 }
 
 class Page1 extends StatelessWidget {
   const Page1({super.key});
+  
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 1920,
-          height: 1080,
-          clipBehavior: Clip.antiAlias,
-          decoration: const BoxDecoration(
-            gradient: RadialGradient(
-              center: Alignment(1,0),
-              radius: 0,
-              colors: [Color(0xFF2C2C2C), Color(0xFF252424)]
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: RadialGradient(
+          center: Alignment.center,
+          radius: 0.8,
+          colors: [
+            Color(0xFF403F3F),
+            Color(0xFF252524),
+          ],
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              "1/1",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontFamily: "ShareTech",
+                fontWeight: FontWeight.w400,
+                height: 0,
+              ),
             ),
-          ),
-          child: Stack(
-            children: [
-              Positioned(
-                left: 217,
-                top: 118,
-                child: Container(
-                  width: 1486,
-                  height: 844,
-                  decoration: ShapeDecoration(
-                    color: const Color(0xFF5F8C93),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
+            const SizedBox(height: 20),
+            Container(
+              width: 1200,
+              height: 600,
+              decoration: BoxDecoration(
+                color: const Color(0xFF5F8C93),
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0xFF608D94).withOpacity(0.5),
+                    spreadRadius: 10,
+                    blurRadius: 20,
+                    offset: const Offset(0, 0),
+                  ),
+                ],
+              ),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "cardFlash",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 200,
+                      fontFamily: "ShareTech",
+                      fontWeight: FontWeight.w400,
+                      height: 0,
                     ),
                   ),
-                ),
-              ),
-              const Positioned(
-                left: 545,
-                top: 416,
-                child: Text(
-                  "Card Flash",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 220,
-                    fontFamily: "Share Tech",
-                    fontWeight: FontWeight.w400,
-                    height: 0,
+                  SizedBox(height: 20),
+                  Text(
+                    "press to begin...",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 45,
+                      fontFamily: "ShareTech",
+                      fontWeight: FontWeight.w400,
+                      height: 0,
+                    ),
                   ),
-                ),
+                ],
               ),
-              const Positioned(
-                left: 802,
-                top: 636,
-                child: Text(
-                  "press to begin...",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 50,
-                    fontFamily: "Share Tech",
-                    fontWeight: FontWeight.w400,
-                    height: 0,
-                  ),
-                ),
-              ),
-              const Positioned(
-                left: 941,
-                top: 58,
-                child: Text(
-                  "1/1",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontFamily: "Share Tech",
-                    fontWeight: FontWeight.w400,
-                    height: 0,
-                  ),
-                ),
-              )
-            ],
-          )
-        )
-      ]
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
